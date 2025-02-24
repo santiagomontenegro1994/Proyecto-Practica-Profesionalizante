c3c50707239342bbc95d3db21404e172608f6d7d
Pachy <pachycat94@gmail.com>
2025-02-24 16:25:44 -0300

    actualizando todos los filtros

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -5,30 +5,49 @@
 $(document).ready(function() { //Se asegura que el DOM este cargado 
 
 
-    function cambiarFiltro(filtro) {
-        // Actualizar el texto del período seleccionado
-        console.log("entre");
-        const periodoElement = document.getElementById('periodo');
-        periodoElement.textContent = `| ${filtro.charAt(0).toUpperCase() + filtro.slice(1)}`;
+});
 
-        // Llamar a la función PHP para obtener los datos
-        obtenerGanancias(filtro);
+
+function cambiarFiltro(filtro, tipo) {
+    // Actualizar el texto del período seleccionado
+    const periodoElement = document.getElementById(`periodo-${tipo}`);
+    if (periodoElement) {
+        periodoElement.textContent = `| ${filtro.charAt(0).toUpperCase() + filtro.slice(1)}`;
     }
 
-    function obtenerGanancias(filtro) {
-        // Hacer una solicitud AJAX al servidor
-        fetch(`ajax.php?accion=obtener_ganancia&filtro=${filtro}`)
+    // Llamar a la función PHP para obtener los datos
+    obtenerDatos(filtro, tipo);
+}
+
+function obtenerDatos(filtro, tipo) {
+    // Hacer una solicitud AJAX al servidor
+    fetch(`select_general.php?accion=obtener_${tipo}&filtro=${filtro}`)
         .then(response => response.json())
         .then(data => {
-            // Actualizar el total de ganancias en la interfaz
-            const totalGananciaElement = document.getElementById('totalGanancia');
-            totalGananciaElement.textContent = `$${data.total}`;
+            // Actualizar los datos en la interfaz según el tipo
+            switch (tipo) {
+                case 'ganancia':
+                    const totalGananciaElement = document.getElementById('totalGanancia');
+                    if (totalGananciaElement) {
+                        totalGananciaElement.textContent = `$${data.total}`;
+                    }
+                    break;
+                case 'turnos':
+                    const totalTurnosElement = document.getElementById('totalTurnos');
+                    if (totalTurnosElement) {
+                        totalTurnosElement.textContent = data.total;
+                    }
+                    break;
+                case 'reportes':
+                    // Aquí puedes actualizar el gráfico de reportes si es necesario
+                    console.log('Datos de reportes:', data);
+                    break;
+                default:
+                    console.error('Tipo no válido:', tipo);
+            }
         })
         .catch(error => console.error('Error:', error));
-    }
-
-
-});
+}
 
 
 
