d6069f6062f1b6367f247e1d4a6f04bbb34a4ec2
Pachy <pachycat94@gmail.com>
2025-02-25 17:17:37 -0300

    acomodando parte del filtro

--- panel_turnos.php
+++ panel_turnos.php
@@ -56,8 +56,6 @@ require ('barraLateral.inc.php'); //Aca uso el encabezaso que esta seccionados e
                       </div>
                       <div class="ps-3">
                           <h6 id="totalTurnos">12</h6>
-                          <span class="text-success small pt-1 fw-bold">90%</span> 
-                          <span class="text-muted small pt-2 ps-1">Resueltos</span>
                       </div>
                   </div>
               </div>
@@ -97,7 +95,8 @@ require ('barraLateral.inc.php'); //Aca uso el encabezaso que esta seccionados e
 
         <!-- Reports -->
         <div class="col-12">
-          <div class="card">
+            <div class="card">
+
               <div class="filter">
                   <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
                   <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
@@ -112,12 +111,70 @@ require ('barraLateral.inc.php'); //Aca uso el encabezaso que esta seccionados e
               </div>
 
               <div class="card-body">
-                  <h5 class="card-title">Reportes <span id="periodo-reportes">/Hoy</span></h5>
+                <h5 class="card-title">Reportes <span id="periodo-reportes">/Hoy</span></h5>
+
                   <!-- Line Chart -->
                   <div id="reportsChart"></div>
+
+                  <script>
+                    document.addEventListener("DOMContentLoaded", () => {
+                      new ApexCharts(document.querySelector("#reportsChart"), {
+                        series: [{
+                          name: 'Baño',
+                          data: [31, 40, 28, 51, 42, 82, 56],
+                        }, {
+                          name: 'Corte',
+                          data: [11, 32, 45, 32, 34, 52, 41]
+                        }, {
+                          name: 'Peinado',
+                          data: [15, 11, 32, 18, 9, 24, 11]
+                        }],
+                        chart: {
+                          height: 350,
+                          type: 'area',
+                          toolbar: {
+                            show: false
+                          },
+                        },
+                        markers: {
+                          size: 4
+                        },
+                        colors: ['#4154f1', '#2eca6a', '#ff771d'],
+                        fill: {
+                          type: "gradient",
+                          gradient: {
+                            shadeIntensity: 1,
+                            opacityFrom: 0.3,
+                            opacityTo: 0.4,
+                            stops: [0, 90, 100]
+                          }
+                        },
+                        dataLabels: {
+                          enabled: false
+                        },
+                        stroke: {
+                          curve: 'smooth',
+                          width: 2
+                        },
+                        xaxis: {
+                          type: 'datetime',
+                          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+                        },
+                        tooltip: {
+                          x: {
+                            format: 'dd/MM/yy HH:mm'
+                          },
+                        }
+                      }).render();
+                    });
+                  </script>
+                  <!-- End Line Chart -->
+
               </div>
-          </div>
-        </div><!-- End Reports -->
+
+            </div>
+        </div>
+        <!-- End Reports -->
 
         <!-- Top Selling -->
         <div class="col-12">
